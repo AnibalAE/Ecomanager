978e058e4c682f212f6e46ffe3032e72ce8ffb43
Marcel <marcmatias@me.com>
2018-03-05 14:08:43 +0000

    primeiro commit

---
+++ blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Predio
+
+
+
+
+# class PostForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'text')
+#         widgets = {
+#             'text': forms.TextInput(attrs={'class': 'materialize-textarea'}),
+#         }
+#
+
+
+
+# class CommentForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Comment
+#         fields = ('author', 'text',)
